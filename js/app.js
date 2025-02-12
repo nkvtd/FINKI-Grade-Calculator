601fe8372a42a3dae64b2d7135bde50274d8f6d3
Tome <149436522+nkvtd@users.noreply.github.com>
2025-02-12 01:57:41 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,10 +2,21 @@
 
 let subjects = [];
 
-async function fetchData(){
+async function fetchData() {
     let result = await fetch("./data.json");
-    subjects = await result;
+    subjects = await result.json();
     console.log(subjects);
+    populateSubjects();
 }
+
 fetchData();
 
+function populateSubjects() {
+    subjects.forEach(subject => {
+        let s = document.createElement("div")
+        s.className="subject"
+        s.title=subject.name
+        s.innerHTML=subject.acronym
+        document.getElementById("subject-selector").appendChild(s)
+    })
+}
